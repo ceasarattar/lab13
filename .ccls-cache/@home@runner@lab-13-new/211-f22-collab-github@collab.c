0611829fb6e4c861a07eb1e518aadf7609f42e60
Ceasar Attar <catta2@uic.edu>
2022-11-17 16:52:47 +0000

    Successfull

---
+++ .ccls-cache/@home@runner@lab-13-new/211-f22-collab-github@collab.c
@@ -0,0 +1,36 @@
+// Collaborative code file, Lab 13
+// 
+// One person in your partner group should clone this repository.
+// Create a function that outputs your name, along with either one thing 
+//    that you learned about git or GitHub or something that gave you trouble
+//    that you would like others to learn from. The function name should be
+//    your NetID combined with your partner's NetID.
+// Feel free to add other things to your function as well, though be mindful
+// of the length of it, given how many students will be collaborating on this code!
+// 
+// Read through the code that is already here before adding your own.
+// Add this code to the repository using git commands.
+// 
+// An example is shown below.
+// 
+// UIC CS 211, Fall 2022
+#include <stdio.h>
+
+void reed_ekidan2() {
+  printf("Dale Reed and Ellen Kidane\n");
+  printf("We learned that git and GitHub are amazing, powerful tools!\n");
+}
+
+void catta2_jalfor3() {
+  printf("Ceasar Attar and Jamani Alford\n");
+  printf("I previously did not know that git is a command within itself and ahd thought that Github is a just a name given to a website that manages repositories. I also did not know that we can collaboratively edit a file using git.\n");
+}
+
+int main()
+{
+    printf("This code has been created by the students of CS 211, Fall 2022!\n\n");
+    
+    reed_ekidan2();
+    
+    return 0;
+}
